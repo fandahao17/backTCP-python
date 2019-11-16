8333697308373d251726b02b06c310c185e0a6a6
Fandahao <fandahao17@mail.ustc.edu.cn>
2019-11-16 23:23:23 +0800

    Finish go-back-n

--- backTCP.py
+++ backTCP.py
@@ -1,5 +1,6 @@
 import os
 import socket
+import threading
 
 from utils import log
 
@@ -104,24 +105,54 @@ def send(data, addr, port):
 
     chunks = [data[x * 64:x * 64 + 64] for x in range((len(data) - 1) // 64 + 1)]
     packets = [BTcpPacket(seq=i & 0xFF, data_off=7, data=chunk) for i, chunk in enumerate(chunks)]
+    data_length = len(packets)
 
     # TODO: "data" is a bytes object
     #       You should split it up into BTcpPacket objects, and call conn.send(pkt) on each one
     # Example: > p = BTcpPacket(data=b"hello")
     #          > conn.send(p)
 
-    # TODO: Delete the following code and add your own
+    N = 5
+    base, next_seq = 0, 0
 
-    for p in packets:
-        conn.send(p)
+    for i in range(N):
+        log('info', 'SEND: Send packet: {}'.format(packets[i]))
+        next_seq += 1
+        conn.send(packets[i])  # Send the first N packets
 
-    # End of your own code
+    conn.settimeout(0.05)
+    while True:
+        try:
+            p = conn.recv()
+        except socket.timeout:
+            # A timeout event must have happened
+            log('info', 'SEND: Timeout for pkt {}'.format(base))
+            for i in range(N):
+                if base + i >= data_length:
+                    break
+                log('info', 'SEND: Send packet: {}'.format(packets[base+i]))
+                conn.send(packets[base + i])  # Resend the N packets
+        else:
+            distance = (p.ack + 256 - base % 256) % 256
+            log('info', 'Distance is {}'.format(distance))
+            if distance >= 0 and distance < N:
+                log('info', 'SEND: Received ack {} when base is {}'.format(p.ack, base))
+                base += (p.ack + 256 - base) % 256 + 1
+                if base >= data_length:
+                    break
+
+                while next_seq < data_length and next_seq != base + N:
+                    log('info', 'SEND: Send packet: {}'.format(packets[next_seq]))
+                    conn.send(packets[next_seq])
+                    next_seq += 1
+
     return
 
 
 def recv(addr, port):
     conn = BTcpConnection('recv', addr, port)
 
+    cur_ack = -1 # Next packet to be received has sequence number 0
     data = b''  # Nothing received yet
 
     # TODO: Call conn.recv to receive packets
@@ -135,9 +166,16 @@ def recv(addr, port):
     conn.settimeout(0.010)  # 10ms timeout
     while True:
         p = conn.recv()
+        log('info', 'RECV: Received packet: {} when cur_ack is {}'.format(p, cur_ack))
         if p is None:  # No more packets
             break
-        data += p.data
+
+        if p.seq == (cur_ack + 1) & 0xFF:
+            log('info', 'RECV: cur_ack becomes {}'.format(cur_ack+1))
+            data += p.data
+            cur_ack = (cur_ack + 1) & 0xff
+
+        conn.send(BTcpPacket(ack=cur_ack))
 
     # End of your own code
 
--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # dd if=/dev/urandom of=input.bin bs=64k count=1
-python3 recv.py -p 6668 output.bin &
-python3 testch.py -p 6668 -P 8003 &
+python3 recv.py -p 6668 output.bin -l debug &
+python3 testch.py -p 6668 -P 8003 -l info &
 sleep 0.1
-python3 send.py -p 8003 input.bin
+python3 send.py -p 8003 input.bin -l debug
 cmp input.bin output.bin
